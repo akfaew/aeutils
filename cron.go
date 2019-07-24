3819d2a61951f8c0f3d6ec801d3a6dadcec6f921
Michal Mazurek <akfaew@gmail.com>
2019-07-24 12:30:46 +0200

    Add ValidateTask

--- cron.go
+++ cron.go
@@ -6,14 +6,24 @@ import (
 )
 
 var (
-	ErrNoCronHeader = errors.New("Cron request does not have the X-Appengine-Cron header")
+	ErrNoCronHeader     = errors.New("Cron request does not have the X-Appengine-Cron header")
+	ErrNoTasknameHeader = errors.New("Task request does not have the X-Appengine-Taskname header")
 )
 
-// ValidateCron returns ErrNoCronHeader if the request was not sent by AppEngine Cron.
+// ValidateCron returns ErrNoCronHeader if the request was not sent by AppEngine.
 // It returns nil otherwise.
 func ValidateCron(r *http.Request) error {
-	if r.Header.Get("X-Appengine-Cron") != "true" {
+	if r.Header.Get("X-Appengine-Cron") == "" {
 		return ErrNoCronHeader
+	}
+	return nil
+}
+
+// ValidateTask returns ErrNoTasknameHeader if the request was not sent by AppEngine.
+// It returns nil otherwise.
+func ValidateTask(r *http.Request) error {
+	if r.Header.Get("X-Appengine-Taskname") == "" {
+		return ErrNoTasknameHeader
 	}
 	return nil
 }
